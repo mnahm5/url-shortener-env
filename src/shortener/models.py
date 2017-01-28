31f2f01ef34651b1fad61562cba8202e533eef0a
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-28 21:19:23 +1100

    making the shortener model

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class ShortenedUrl(models.Model):
+    url = models.CharField(max_length=220)
+    shortCode = models.CharField(max_length=20, unique=True)
+
+    def __unicode__(self):
+        return str(self.url)
+
+
+
+
 # Create your models here.
