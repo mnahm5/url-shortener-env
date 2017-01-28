ab8f7e74b1ccd6979fb657ced092b963ea813b2d
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-28 23:08:50 +1100

    making a refresh management method

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -4,11 +4,31 @@ from django.db import models
 
 from .utils import code_generated, create_shortcode
 
+class ShortenedUrlManager(models.Manager):
+
+    def all(self, *args, **kwargs):
+        qs = super(ShortenedUrlManager, self).all(*args, **kwargs)
+        qs = qs.filter(active=True)
+        return qs
+
+    def refresh_short_codes(self):
+        qs = ShortenedUrl.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shorCode = create_shortcode(q)
+            print(q.shorCode)
+            q.save()
+            new_codes += 1
+        return "New Codes made : {i}".format(i=new_codes)
+
 class ShortenedUrl(models.Model):
     url = models.CharField(max_length=220)
     shortCode = models.CharField(max_length=20, unique=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    objects = ShortenedUrlManager()
 
     def __unicode__(self):
         return str(self.url)
