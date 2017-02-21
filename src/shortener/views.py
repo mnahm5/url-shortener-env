176c1dd7a57c5cdfb756c2a63e6fec762cf8c820
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-02-21 22:26:03 +1100

    Django Forms

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,21 +1,31 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .forms import SubmitUrlForm
 from .models import ShortenedUrl
 
 # Create your views here
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "shortener/home.html", {
-
-        })
+        form = SubmitUrlForm()
+        context = {
+            "title": "Submit URL",
+            "form": form
+        }
+        return render(request, "shortener/home.html", context)
 
     def post(self, request, *args, **kwargs):
-        print(request.POST.get("url"))
-        return render(request, "shortener/home.html", {
+        form = SubmitUrlForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
 
-        })
+        context = {
+            "title": "Submit URL",
+            "form": form
+        }
+        return render(request, "shortener/home.html", context)
 
 class ShortenedUrlRedirectView(View):
     def get(self, request, shortcode=None, *args, **kwargs):
